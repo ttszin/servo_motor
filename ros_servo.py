f874128a6929730091f33f677da3b63f43911547
ttszin <matheuskrskr@gmail.com>
2024-08-05 17:19:11 -0300

    jeston servo

--- ros_servo.py
+++ ros_servo.py
@@ -47,6 +47,6 @@ if __name__ == "__main__":
     # rospy.Subscriber("/mavros/state", State, state_callback)
     
     # Exemplo: Ativa o servo no canal 7 com um valor PWM de 1500
-    activate_servo(7, 1500):
+    activate_servo(7, 1500)
     rospy.loginfo("Servo ativado com sucesso!")
     
