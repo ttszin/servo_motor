cd98342e376476d4af094a0783ec48f00f0f66b4
ttszin <matheuskrskr@gmail.com>
2024-08-04 23:34:06 -0300

    jeston servo

--- jetson_servo.py
+++ jetson_servo.py
@@ -1,34 +1,31 @@
 import rospy
-from mavros_msgs.srv import CommandLong, CommandLongRequest
+from mavros_msgs.srv import CommandLong
 
 def send_set_servo_command(servo_number, pwm_value):
     rospy.wait_for_service('/mavros/cmd/command')
     try:
-        command_service = rospy.ServiceProxy('/mavros/cmd/command', CommandLongRequest)
-        request = CommandLongRequest(
-            broadcast=False,
-            command=183,  # MAV_CMD_DO_SET_SERVO
-            confirmation=0,
-            param1=servo_number,  # Servo number (1 for first servo, 2 for second, etc.)
-            param2=pwm_value,     # PWM value (e.g., 1100 to 1900)
-            param3=0,
-            param4=0,
-            param5=0,
-            param6=0,
-            param7=0
+        command_service = rospy.ServiceProxy('/mavros/cmd/command', CommandLong)
+        response = command_service(
+            False,        # broadcast
+            183,          # MAV_CMD_DO_SET_SERVO
+            0,            # confirmation
+            servo_number, # param1: Servo number (1 for first servo, 2 for second, etc.)
+            pwm_value,    # param2: PWM value (e.g., 1100 to 1900)
+            0,            # param3
+            0,            # param4
+            0,            # param5
+            0,            # param6
+            0             # param7
         )
-        response = command_service(request)
         return response.success
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed: %s" % e)
         return False
-        
 
-    def delivery(self, servo_channel=7, pwm_values=[2000, 700]):
-        for pwm in pwm_values:
-            self.send_set_servo_command(servo_channel, pwm)
-            rospy.Rate(20).sleep()
-
+def delivery(servo_channel=7, pwm_values=[2000, 700]):
+    for pwm in pwm_values:
+        send_set_servo_command(servo_channel, pwm)
+        rospy.Rate(20).sleep()
 
 # Inicializa o nó ROS
 rospy.init_node('servo_control_node')
