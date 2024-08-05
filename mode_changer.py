256c29c2f38a6729cc9b52708051fc10c9a1e7af
ttszin <matheuskrskr@gmail.com>
2024-08-05 00:28:27 -0300

    jeston servo

--- mode_changer.py
+++ mode_changer.py
@@ -1,3 +1,6 @@
+import rospy
+from mavros_msgs.srv import CommandLong,CommandLongRequest
+
 def set_custom_mode(custom_mode: str = "GUIDED") -> str:
         """This method set a custom mode to UAV
 
